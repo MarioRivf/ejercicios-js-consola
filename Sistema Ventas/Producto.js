447472fb791ee86a2ede37cc75dbee7c9cffea0b
MarioRivas <mario.rivas2497gmail.com>
2024-10-31 17:03:53 -0600

    Mejoras dentro del sistema de ventas
    - Se agrego metodo para agregar los productos a la orden y tambien se mejoro la presentacion de la orden

--- Sistema Ventas/Producto.js
+++ Sistema Ventas/Producto.js
@@ -41,11 +41,58 @@ class Orden{
     constructor(){
         this._id_orden= ++Orden.contadorOrden;
         this._productos = [];
-        this._contadorProductosAgregados = 0;
+        //this._contadorProductosAgregados = 0;
+    }
+
+    get idOrden(){
+        return this._id_orden;
+    }
+
+    agregarProducto(producto){
+        if(this._productos.length < Orden.MAX_PRODUCTO){
+            this._productos.push(producto);
+            //this._productos[this._contadorProductosAgregados++] = producto;
+        }
+        else{
+            console.log('No se puede agregar mas productos')
+        }
+    }
+
+    calcularTotal(){
+        let totalVenta = 0;
+        for(let producto of this._productos){
+            totalVenta += producto.precio
+        }
+
+        return totalVenta;
+    }
+
+    mostrarOrden(){
+        let productoOrden = '';
+        for(let producto of this._productos){
+            productoOrden += '\n{' + producto.toString() + '}';
+        }
+
+        console.log(`Orden: ${this._id_orden} Total: C$${this.calcularTotal()} Productos: ${productoOrden} `);
     }
 }
 
 let producto1 = new Producto("Leche", 20);
 let producto2 = new Producto("Cafe", 3);
-console.log(producto1.toString());
-console.log(producto2.toString());
+//console.log(producto1.toString());
+//console.log(producto2.toString());
+
+let orden1 = new Orden();
+orden1.agregarProducto(producto1);
+orden1.agregarProducto(producto2);
+
+orden1.mostrarOrden();
+
+let orden2 = new Orden();
+let producto3 = new Producto('Producto prueba 3', 100);
+
+orden2.agregarProducto(producto3);
+orden2.agregarProducto(producto1);
+orden2.agregarProducto(producto2);
+
+orden2.mostrarOrden();
